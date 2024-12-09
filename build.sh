473b5c5ec4a9c153cfc9c64e44b644796527c35e
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-12-09 11:09:53 +0800

    Update

--- build.sh
+++ build.sh
@@ -32,12 +32,12 @@ cp -r ../cbinding/cbinding-library-Linux-x86_64/include ../cbinding/install
 uv run scripts/build_ffi.py
 mv gitcg/_gitcg_cffi.py src/gitcg
 
-cp ../cbinding/cbinding-library-Linux-x86_64/lib/libgitcg.so gitcg && uv build && rm gitcg/libgitcg.*
+cp ../cbinding/cbinding-library-Linux-x86_64/lib/libgitcg.so src/gitcg && uv build && rm src/gitcg/libgitcg.*
 uv run wheel tags --remove --platform-tag=manylinux_2_27_x86_64 dist/*any.whl
-cp ../cbinding/cbinding-library-Windows-x86_64/lib/gitcg.lib gitcg && uv build && rm gitcg/gitcg.*
+cp ../cbinding/cbinding-library-Windows-x86_64/bin/gitcg.dll src/gitcg && uv build && rm src/gitcg/gitcg.*
 uv run wheel tags --remove --platform-tag=windows_x86_64 dist/*any.whl
-cp ../cbinding/cbinding-library-macOS-x86_64/lib/libgitcg.dylib gitcg && uv build && rm gitcg/libgitcg.*
+cp ../cbinding/cbinding-library-macOS-x86_64/lib/libgitcg.dylib src/gitcg && uv build && rm src/gitcg/libgitcg.*
 uv run wheel tags --remove --platform-tag=macosx_12_0_x86_64 dist/*any.whl
-cp ../cbinding/cbinding-library-macOS-arm64/lib/libgitcg.dylib gitcg && uv build && rm gitcg/libgitcg.*
+cp ../cbinding/cbinding-library-macOS-arm64/lib/libgitcg.dylib src/gitcg && uv build && rm src/gitcg/libgitcg.*
 uv run wheel tags --remove --platform-tag=macosx_12_0_arm64 dist/*any.whl
 popd
