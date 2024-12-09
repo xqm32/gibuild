2db32c69397d913c0c0b1f901c9c5fd78aa87a59
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2024-12-09 14:51:17 +0800

    Update

--- build.sh
+++ build.sh
@@ -32,12 +32,20 @@ cp -r ../cbinding/cbinding-library-Linux-x86_64/include ../cbinding/install
 uv run scripts/build_ffi.py
 mv gitcg/_gitcg_cffi.py src/gitcg
 
+VER=$(objdump -p ../cbinding/cbinding-library-Linux-x86_64/lib/libgitcg.so |
+	awk '/GLIBC_/ {gsub(/^GLIBC_/, "", $NF); gsub(/\./, "_", $NF); print $NF}' |
+	sort -V |
+	tail -n 1)
+
 cp ../cbinding/cbinding-library-Linux-x86_64/lib/libgitcg.so src/gitcg && uv build && rm src/gitcg/libgitcg.*
-uv run wheel tags --remove --platform-tag=manylinux_2_27_x86_64 dist/*any.whl
+uv run wheel tags --remove --platform-tag=manylinux_${VER}_x86_64 dist/*any.whl
+
 cp ../cbinding/cbinding-library-Windows-x86_64/bin/gitcg.dll src/gitcg && uv build && rm src/gitcg/gitcg.*
 uv run wheel tags --remove --platform-tag=windows_x86_64 dist/*any.whl
+
 cp ../cbinding/cbinding-library-macOS-x86_64/lib/libgitcg.dylib src/gitcg && uv build && rm src/gitcg/libgitcg.*
 uv run wheel tags --remove --platform-tag=macosx_12_0_x86_64 dist/*any.whl
+
 cp ../cbinding/cbinding-library-macOS-arm64/lib/libgitcg.dylib src/gitcg && uv build && rm src/gitcg/libgitcg.*
 uv run wheel tags --remove --platform-tag=macosx_12_0_arm64 dist/*any.whl
 popd
